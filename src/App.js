8436af4164cac030bbf4331e7ab2188cc9b5f78f
Zulqarnain Chohan <zulqarnainc67@gmail.com>
2023-04-05 00:45:18 -0700

    initil commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import "./login.css"
+import { useFormik } from 'formik'
+import { userSchema } from './Schemas'
+const initialValues = {
+  name:"",
+  email:"",
+  password:"",
+  confirm_password:"",
+}
+const App = () => {
+   const { values,errors,touched,handleBlur,handleChange,handleSubmit } = useFormik({
+     initialValues:initialValues,
+     validationSchema:userSchema,
+    onSubmit:(values,action) => {
+      console.log(values);
+      action.resetForm()
+    }
+   })
+  //  console.log(formik); it will provide all the packages of formic
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+ 
+      <div className='container'>
+        <form onSubmit={handleSubmit}>
+         <div className="logincontainer">
+          <h2>Login</h2>
+          
+          <p>Username</p>
+          <input 
+             type="text"
+             name='name'
+             autoComplete='off'
+             value={values.name}
+             onChange ={handleChange}
+             onBlur = {handleBlur}
+           />
+          {errors.name && touched.name ?(<p className='error'>{errors.name}</p>):null}
+          
+          <p>Email</p>
+          <input 
+            type="email"
+            name='email'
+            autoComplete='off'
+            value={values.email}
+            onChange ={handleChange}
+            onBlur = {handleBlur}
+            
+           />
+           {errors.email && touched.email ? (<p className='error'>{errors.email}</p>): null}
+          <p>Password</p>
+          <input
+            type="password" 
+            name='password'
+            autoComplete='off'
+            value={values.password}
+            onChange ={handleChange}
+            onBlur = {handleBlur}
+           />
+           {errors.password && touched.password ?(<p className='error'>{errors.password}</p>): null}
+
+          <p>Confirm Password</p>
+          <input
+            type="password" 
+            name='confirm_password'
+            autoComplete='off'
+            value={values.confirm_password}
+            onChange ={handleChange}
+            onBlur = {handleBlur}
+          
+          />
+          {errors.confirm_password && touched.confirm_password ?( <p className='error'>{errors.confirm_password}</p>): null}
+          <button type='submit'>Login</button>
+         </div>
+        </form>
     </div>
-  );
+    
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Schemas.jsx
@@ -0,0 +1,8 @@
+import * as Yup from "yup" 
+export const userSchema = Yup.object({
+    name:Yup.string().min(3).max(26).required("Please enter your name"),
+    email:Yup.string().email().required("Please enter your email"),
+    password:Yup.string().min(6).required("Please enter your password"),
+    confirm_password:Yup.string().required()
+    .oneOf([Yup.ref("password"),null], "Password must match")
+})
